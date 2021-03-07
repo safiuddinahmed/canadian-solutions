f2d6164136a1675a9deb42061cf6de48549958d3
safiuddinahmed <safiuddinahmed.mohammad@gmail.com>
2021-03-07 16:35:49 -0500

    Added production stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -16,9 +17,9 @@ app.use(
 
 app.use(cors());
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the CanadianSolutions API" })
-);
+// app.get("/", (req, res) =>
+//   res.json({ msg: "Welcome to the CanadianSolutions API" })
+// );
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
@@ -27,6 +28,16 @@ app.use("/api/business", require("./routes/business"));
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/comments", require("./routes/comments"));
 app.use("/api/blog", require("./routes/blog"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
